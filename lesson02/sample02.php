ef9a489a96dcff7a237d6c06831dac86a2625503
katayama <tattu.0310@gmail.com>
2022-02-22 16:59:57 +0900

    select

--- lesson02/sample02.php
+++ lesson02/sample02.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     $db = new mysqli("localhost:8889", "root", "root", "mydb");
     //$db->query('drop table test1');
-    $db->query('create table test1(id INT)');
+    $records = $db->query('select*from my_items');
+    if($records){
+        while($record = $records->fetch_assoc()){
+            echo $record['item_name'] . '<br>';
+        }
+    }
     echo '削除';
     ?>
+
 </body>
+
 </html>
